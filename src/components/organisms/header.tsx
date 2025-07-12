52b18ef3553e5d0b36e241be924e5e91b57ade02
Dwi Rijal Giri Prabowo (Rijal) <arrijalprabowo.ap@gmail.com>
2025-07-12 02:05:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenuSubContent,
   DropdownMenuSubTrigger,
   DropdownMenuPortal,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";

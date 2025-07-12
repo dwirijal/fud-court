a28f72450f2d514501b76d88b4d325be58c6fb15
Dwi Rijal Giri Prabowo (Rijal) <arrijalprabowo.ap@gmail.com>
2025-07-12 21:05:45 +0000

    [04:03:40.894] Running build in Washington, D.C., USA (East) – iad1 [04:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default async function Home() {
           </div>
 
           <div className="mt-16">
-            <MarketCarousel data={cryptoData} />
+            <MarketCarousel data={cryptoData || []} />
           </div>
         </div>
       </section>

5fd10bc27a901cdb8e47fa25b4fa17a09b4809e4
Dwi Rijal Giri Prabowo (Rijal) <arrijalprabowo.ap@gmail.com>
2025-07-12 09:48:22 +0000

    npm i @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import { AppLayout } from '@/components/organisms/app-layout';
 import { Inter, Space_Grotesk } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -37,6 +38,7 @@ export default function RootLayout({
           {children}
         </AppLayout>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );

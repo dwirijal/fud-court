7b8697a313ed8b68436fc608fd2338bf450aff7d
Dwi Rijal Giri Prabowo (Rijal) <arrijalprabowo.ap@gmail.com>
2025-07-16 17:46:03 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/molecules/nft-card.tsx
+++ src/components/molecules/nft-card.tsx
@@ -61,7 +61,7 @@ export function NftCard() {
             className="aspect-[4/5] relative w-full overflow-hidden rounded-lg"
           >
             <Image
-              src="https://placehold.co/600x600.png"
+              src="https://images.unsplash.com/photo-1719433203940-a9329d15d3e9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwzfHxGRU5ORUMlMjBGT1h8ZW58MHx8fHwxNzUyNjg3OTA5fDA&ixlib=rb-4.1.0&q=80&w=1080"
               alt="CryptoPulse Genesis NFT"
               fill
               className="object-cover"

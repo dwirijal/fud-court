74357bb51a38224d706a56596eae2488d6806b0b
Dwi Rijal Giri Prabowo (Rijal) <arrijalprabowo.ap@gmail.com>
2025-07-16 13:33:31 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="contai

--- src/app/markets/page.tsx
+++ src/app/markets/page.tsx
@@ -129,7 +129,7 @@ function MarketIndicatorsClient({ marketData }: { marketData: any }) {
   }
 
   return (
-    <div className="container mx-auto px-4 py-12 md:py-24">
+    <div className="container mx-auto px-4 py-8 md:py-12">
       <Breadcrumb className="mb-8">
         <BreadcrumbList>
           <BreadcrumbItem>

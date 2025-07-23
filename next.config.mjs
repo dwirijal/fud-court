8c61eb080381823b393777107b84580e72a9c48d
Dwi Rijal Giri Prabowo (Rijal) <arrijalprabowo.ap@gmail.com>
2025-07-23 07:36:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cryptologos.cc',
-        port: '',
-        pathname: '/logos/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.coingecko.com',
-        port: '',
-        pathname: '/coins/images/**',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cryptologos.cc',
+                port: '',
+                pathname: '/logos/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'coin-images.coingecko.com',
+                port: '',
+                pathname: '/coins/images/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'assets.coingecko.com',
+                port: '',
+                pathname: '/coins/images/**',
+            }
+        ]
+    }
 };
 
 export default nextConfig;
